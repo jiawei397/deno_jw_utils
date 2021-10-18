6ff5a706ff3d5f463152762dea94a60ba8eb3435
jia wei <jw397@126.com>
2021-10-18 14:27:17 +0800

    style: lint

--- src/tools/ajax.ts
+++ src/tools/ajax.ts
@@ -1,37 +1,35 @@
-import { BaseAjax, AjaxConfig } from "../../deps.ts";
+import { AjaxConfig, BaseAjax } from "../../deps.ts";
 import { md5 } from "./utils.ts";
 
 class Ajax extends BaseAjax {
-
-    protected getUniqueKey(config: AjaxConfig) {
-        const headers: any = config.headers;
-        const key = (config.baseURL || "") + config.url + config.method +
-            (config.data ? JSON.stringify(config.data) : "");
-        let lastKey = key;
-        if (headers) {
-            const cookie = headers['cookie'] || headers.get?.("cookie") || "";
-            lastKey += cookie;
-        }
-        return md5(lastKey);
+  protected getUniqueKey(config: AjaxConfig) {
+    const headers: any = config.headers;
+    const key = (config.baseURL || "") + config.url + config.method +
+      (config.data ? JSON.stringify(config.data) : "");
+    let lastKey = key;
+    if (headers) {
+      const cookie = headers["cookie"] || headers.get?.("cookie") || "";
+      lastKey += cookie;
     }
+    return md5(lastKey);
+  }
 
+  /**
+   * 处理消息，具体实现可以覆盖此项
+   */
+  protected handleMessage(msg: string) {
+    // console.log("handleMessage", msg);
+    // super.handleMessage(msg);
+  }
 
-    /**
-     * 处理消息，具体实现可以覆盖此项
-     */
-    protected handleMessage(msg: string) {
-        // console.log("handleMessage", msg);
-        // super.handleMessage(msg);
-    }
-
-    /**
-     * 处理错误请求
-     */
-    protected handleErrorResponse(response: Response) {
-        // console.error(
-        //     `HTTP error, status = ${response.status}, statusText = ${response.statusText}`,
-        // );
-    }
+  /**
+   * 处理错误请求
+   */
+  protected handleErrorResponse(response: Response) {
+    // console.error(
+    //     `HTTP error, status = ${response.status}, statusText = ${response.statusText}`,
+    // );
+  }
 }
 
 export const ajax = new Ajax();
