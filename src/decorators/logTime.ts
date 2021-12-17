6661a4e55fc8a309d1b81d4eb3bf747d725ecb10
jia wei <jw397@126.com>
2021-12-17 16:17:09 +0800

    feat: log controller name

--- src/decorators/logTime.ts
+++ src/decorators/logTime.ts
@@ -1,28 +1,35 @@
 // deno-lint-ignore-file no-explicit-any
 import { Logger } from "../types.ts";
 
-export function logTime(str: string, options: {
+export function logTime(options: {
+  str?: string;
   msg?: string;
   logger?: Logger;
   level?: "debug" | "info" | "warn" | "error";
 } = {}) {
-  const { logger = console, msg, level = "debug" } = options;
+  const { logger = console, msg, level = "debug", str = "take up time" } =
+    options;
   return function (
-    _target: any,
+    target: any,
     propertyKey: string,
     descriptor: PropertyDescriptor,
-  ) {
+  ): MethodDecorator {
     const originalMethod = descriptor.value;
     descriptor.value = async function (...args: any[]) {
       const start = Date.now();
       const result = await originalMethod.apply(this, args);
       logger[level](
-        str,
-        `${propertyKey +
-          (msg ? ` ${msg}` : "")}, take up time: ${(Date.now() - start) /
-          1000} s`,
+        `[${target.constructor.name}]`,
+        `${
+          propertyKey +
+          (msg ? ` ${msg}` : "")
+        }, ${str}: ${
+          (Date.now() - start) /
+          1000
+        } s`,
       );
       return result;
     };
+    return target;
   };
 }
